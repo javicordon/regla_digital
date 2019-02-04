dac05a32a0e704a4664df116f7dc7e8102cd24e0
Javier Cordon <javicordon@gmail.com>
2019-02-04 19:47:28 -0300

    Mejaro de algoritmo arcotangente mediante aproximacion polinomica. Se mejora de 47us a 1.5us

--- example/src/main.c
+++ example/src/main.c
@@ -20,6 +20,8 @@ static volatile uint8_t Burst_Mode_Flag = 0, Sample_Time_sec = 10;
 static volatile uint8_t channelTC, dmaChannelNum;
 uint32_t DMAbuffer;
 #define PI 3.14159265
+#define PI_2 1.570796325
+#define PI2 6.2831853
 
 
 /*****************************************************************************
@@ -51,6 +53,8 @@ static void prvSetupHardware(void)
     Chip_SCU_PinMux(2,11,SCU_MODE_INACT|SCU_MODE_INBUFF_EN,0); //LED 2
     Chip_SCU_PinMux(2,13,SCU_MODE_INACT|SCU_MODE_INBUFF_EN,0); //LED 3
 
+    Chip_SCU_PinMux(6,12,SCU_MODE_INACT|SCU_MODE_INBUFF_EN,0); //GPIO 8
+
     //Entradas
     Chip_SCU_PinMux(1,0,SCU_MODE_INACT|SCU_MODE_INBUFF_EN,0); //TEC1
     Chip_SCU_PinMux(1,1,SCU_MODE_INACT|SCU_MODE_INBUFF_EN,0); //TEC2
@@ -65,6 +69,8 @@ static void prvSetupHardware(void)
     Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT,1,11); //LED 2
     Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT,1,12); //LED 3
 
+    Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT,2,8); //GPI0 8
+
     //MUX Entrada
     Chip_GPIO_SetPinDIRInput(LPC_GPIO_PORT,0,4); //TEC1
     Chip_GPIO_SetPinDIRInput(LPC_GPIO_PORT,0,8); //TEC2
@@ -78,6 +84,7 @@ static void prvSetupHardware(void)
     Chip_GPIO_SetPinState(LPC_GPIO_PORT,0,14,FALSE); //LED 1
     Chip_GPIO_SetPinState(LPC_GPIO_PORT,1,11,FALSE); //LED 2
     Chip_GPIO_SetPinState(LPC_GPIO_PORT,1,12,FALSE); //LED 3
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT,2,8,FALSE); //GPIO 8
 
 }
 
@@ -158,6 +165,148 @@ static void App_print_ADC_value(uint16_t si, uint16_t co, int cont,uint16_t* pos
 }
 
 
+// Aproximacion polinomial de arcotangente entre -1,1.
+// Error maximo < 0.005 (o 0.29 grados)
+float ApproxAtan(float z)
+{
+    const float n1 = 0.97239411f;
+    const float n2 = -0.19194795f;
+    return (n1 + n2 * z * z) * z;
+}
+
+float ApproxAtan2(float y, float x)
+{
+    if (x != 0.0f)
+    {
+        if (fabsf(x) > fabsf(y))
+        {
+            const float z = y / x;
+            if (x > 0.0)
+            {
+                // atan2(y,x) = atan(y/x) if x > 0
+                return ApproxAtan(z);
+            }
+            else if (y >= 0.0)
+            {
+                // atan2(y,x) = atan(y/x) + PI if x < 0, y >= 0
+                return ApproxAtan(z) + PI;
+            }
+            else
+            {
+                // atan2(y,x) = atan(y/x) - PI if x < 0, y < 0
+                return ApproxAtan(z) - PI;
+            }
+        }
+        else // Use property atan(y/x) = PI/2 - atan(x/y) if |y/x| > 1.
+        {
+            const float z = x / y;
+            if (y > 0.0)
+            {
+                // atan2(y,x) = PI/2 - atan(x/y) if |y/x| > 1, y > 0
+                return -ApproxAtan(z) + PI_2;
+            }
+            else
+            {
+                // atan2(y,x) = -PI/2 - atan(x/y) if |y/x| > 1, y < 0
+                return -ApproxAtan(z) - PI_2;
+            }
+        }
+    }
+    else
+    {
+        if (y > 0.0f) // x = 0, y > 0
+        {
+            return PI_2;
+        }
+        else if (y < 0.0f) // x = 0, y < 0
+        {
+            return -PI_2;
+        }
+    }
+    return 0.0f; // x,y = 0. Could return NaN instead.
+}
+
+static void arctan(uint16_t si, uint16_t co, int cont,uint16_t* pos_1b,int32_t* pos_2,uint16_t* promed,int32_t* med5f,int32_t* med5)
+{
+    //Imprime valor ADC de 0 a 1023 niveles por los 10 bit
+    //DEBUGOUT("%04d\r\n", data);
+    //Realiza la conversion de niveles a 3.3V
+
+    float tan_yx;
+    float y, x, res, resf,tantan;
+    int32_t seno,coseno, pos_1;
+    y=si-*promed;
+    seno=y;
+    //y=y/512;
+    x=co-*promed;
+    coseno=x;
+    //x=co/512;
+    //if(x==0) x=1;
+    tan_yx=y/x;
+
+//Chip_GPIO_SetPinState(LPC_GPIO_PORT,2,8,TRUE); //GPIO 8
+    //res= atan(tan_yx)*180000/PI;//Uso 180 000, en vez de 180, para incluir los decimales en entero
+	//res= atan2(y,x)*180000/PI;//Uso 180 000, en vez de 180, para incluir los decimales en entero
+	//res = 0.9724*tan_yx-0.1919*tan_yx*tan_yx*tan_yx;
+    res = ApproxAtan2(y,x);
+    (res<0)?res+=PI2:res;
+    res = res*180000/PI;
+
+//Chip_GPIO_SetPinState(LPC_GPIO_PORT,2,8,FALSE); //GPIO 8
+
+    //tantan=tan_yx*1000;
+    //res=resf;
+
+    if(si>*promed&&co>*promed) {
+        pos_1=0;
+        //*med5f=res;
+        //if(res<-1) *med5f=res+180000;
+    }
+    if(si>*promed&&co<=*promed) {
+        pos_1=1;
+        //if(res<-1) *med5f=res+180000;
+    }
+    if(si<=*promed&&co<=*promed) {
+        pos_1=2;
+        //*med5f=res+180000;
+        //if(res<-1) *med5f=res+360000;
+    }
+    if(si<=*promed&&co>*promed) {
+        pos_1=3;
+        //*med5f=res+360000;
+    }
+
+
+    if((*pos_1b==3)&&(pos_1==0)) {
+        *pos_2=*pos_2+1;
+        *pos_1b=pos_1;
+    };
+    if((*pos_1b==0)&&(pos_1==3)) {
+        *pos_2=*pos_2-1;
+        *pos_1b=pos_1;
+    };
+
+    //*med5f=(*med5f)*5000/360000;
+    *med5f=res*5000/360000;
+
+    if(*pos_2>=0) *med5=*pos_2*5000+*med5f;
+        else *med5=*pos_2*5000+*med5f-5000;
+
+    //result = 2*3.1415;
+    //result = cos(1.5708);
+    //result=data*3.3/1023;
+    //DEBUGOUT("%04d\r\n", result);
+    //DEBUGOUT("%f\r\n", result);
+
+    //printf(" Sin:%i\r\n Cos:%i\r\n ArcTan:%i\r\n",seno,coseno,res);
+    printf("%f\t%f\t%f\t%i\t%i\t%i\t%i\t%i\r\n",y,x,res,pos_1,*pos_2,*promed,*med5f,*med5);
+    *pos_1b=pos_1;
+
+    /* Delay */
+    vTaskDelay((cont)/portTICK_RATE_MS);
+}
+
+
 static void vLectorTEC1(void *pvParameters) {
     int count=0,cont=500;
 
@@ -177,12 +326,12 @@ static void vLectorTEC1(void *pvParameters) {
              vTaskDelay(300/portTICK_RATE_MS);
         }
            xQueueSendToBack(Time,&count,portMAX_DELAY);
-           vTaskDelay(10/portTICK_RATE_MS);
+           vTaskDelay(100/portTICK_RATE_MS);
            //Libera cola Time
            xQueueReceive (Time,&count,portMAX_DELAY);
     }
 }
-static void vLectorTEC2(void *pvParameters) {
+static void adc_tec(void *pvParameters) {
     int cont=5, count=0, max=511,min=511;
     uint16_t dataADC0,dataADC1;
     int32_t* med5 = (int32_t*)calloc(1, sizeof(int32_t));
@@ -201,7 +350,6 @@ static void vLectorTEC2(void *pvParameters) {
             }
             if (count==1){
                 //Empiezo conversion A/D mientras estoy con TEC 1 Activa
-                /* Get  adc value until get 'x' character */
 
                     /* Espera de conversion A/D */
                     while (Chip_ADC_ReadStatus(_LPC_ADC_ID0, _ADC_CHANNLE0, ADC_DR_DONE_STAT) != SET &&Chip_ADC_ReadStatus(_LPC_ADC_ID1, _ADC_CHANNLE1, ADC_DR_DONE_STAT) != SET) {}
@@ -219,8 +367,43 @@ static void vLectorTEC2(void *pvParameters) {
             }
             //Si envie 0 no he presionado TEC1, no hago nada.
             if (count==0){
+            	vTaskDelay(10/portTICK_RATE_MS);
             }
 
+            //vTaskDelay(10/portTICK_RATE_MS);
+        }
+}
+
+static void adc_task(void *pvParameters) {
+    int cont=5, count=0, max=511,min=511;
+    uint16_t dataADC0,dataADC1;
+    int32_t* med5 = (int32_t*)calloc(1, sizeof(int32_t));
+    int32_t* med5f = (int32_t*)calloc(1, sizeof(int32_t));
+    uint32_t* medq5 = (uint32_t*)calloc(1, sizeof(uint32_t));
+    uint16_t* pos_1b = (uint16_t*)calloc(1, sizeof(uint16_t));
+    uint16_t* promed = (uint16_t*)calloc(1, sizeof(uint16_t));
+        while (1) {
+            /* Empiezo conversion A/D solo si no hay modo Burst */
+			Chip_ADC_SetStartMode(_LPC_ADC_ID0, ADC_START_NOW, ADC_TRIGGERMODE_RISING);
+			Chip_ADC_SetStartMode(_LPC_ADC_ID1, ADC_START_NOW, ADC_TRIGGERMODE_RISING);
+
+			//Empiezo conversion A/D
+Chip_GPIO_SetPinState(LPC_GPIO_PORT,2,8,TRUE); //GPIO 8
+			/* Espera de conversion A/D */
+			while (Chip_ADC_ReadStatus(_LPC_ADC_ID0, _ADC_CHANNLE0, ADC_DR_DONE_STAT) != SET &&Chip_ADC_ReadStatus(_LPC_ADC_ID1, _ADC_CHANNLE1, ADC_DR_DONE_STAT) != SET) {}
+			/* Leo valor ADC */
+			Chip_ADC_ReadValue(_LPC_ADC_ID0, _ADC_CHANNLE0, &dataADC0);
+			/* Leo valor ADC */
+			Chip_ADC_ReadValue(_LPC_ADC_ID1, _ADC_CHANNLE1, &dataADC1);
+
+
+			/* Imprimo valor ADC */
+			if(dataADC0>max) max=dataADC0;
+			if(dataADC0<min) min=dataADC0;
+			*promed=max+min;
+			*promed=(*promed)/2;
+			arctan(dataADC0,dataADC1, 5,pos_1b,medq5,promed,med5f,med5);
+Chip_GPIO_SetPinState(LPC_GPIO_PORT,2,8,FALSE); //GPIO 8
             //vTaskDelay(10/portTICK_RATE_MS);
         }
 }
@@ -251,11 +434,10 @@ int main(void)
     /* TASK creation xTaskCreate (Puntero, String descriptivo, Tamaño del Stack, parámetros de la función void* es puntero) */
     //de cualquier cosa que hay que castear, prioridad.
 
-    xTaskCreate(vLectorTEC1, "vLectorTEC1", configMINIMAL_STACK_SIZE,
-                NULL, (tskIDLE_PRIORITY + 2UL), (TaskHandle_t *) NULL);
+    //xTaskCreate(vLectorTEC1, "vLectorTEC1", configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 2UL), (TaskHandle_t *) NULL);
+    //xTaskCreate(adc_tec, "adc_tec", 1024, NULL, (tskIDLE_PRIORITY + 4UL), (TaskHandle_t *) NULL);
 
-    xTaskCreate(vLectorTEC2, "vLectorTEC2", 1024,
-            NULL, (tskIDLE_PRIORITY + 1UL), (TaskHandle_t *) NULL);
+    xTaskCreate(adc_task, "adc_task", 1024, NULL, (tskIDLE_PRIORITY + 4UL), (TaskHandle_t *) NULL);
 
     /* Start the scheduler */
     vTaskStartScheduler();
